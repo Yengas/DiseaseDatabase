1548967cbb3f91fec6f3575925d9e3697fec9a55
Yengas <yigitcan@hotmail.com.tr>
2016-12-27 06:10:59 +0300

    Starting up the GUI application.

--- __main__.py
+++ __main__.py
@@ -1,14 +1,12 @@
-from WebMD.reader import readDatabase
-from WebMD.diagnose import SimpleDiagnoser
+import sys
+from GUI.About import About
+from GUI.Diagnose import Diagnose
+from PyQt5.QtWidgets import QApplication
 
-if __name__ == "__main__":
-    parts = readDatabase('./data')
-    picked = [parts[0].symptoms()[1], parts[2].symptoms()[1]]
-    diagnoser = SimpleDiagnoser()
 
-    for symptom in picked:
-        diagnoser.update(symptom)
-
-    conditions = diagnoser.diagnose()
-    for condition in conditions:
-        print(condition.id, condition.name, condition.weight, condition.about)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    diagnose = Diagnose()
+    about = About(diagnose)
+    about.show()
+    sys.exit(app.exec_())
